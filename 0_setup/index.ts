0b4c9172c878807a88183e0de9568e27159ce068
kitravee <kitravee.swk@gmail.com>
2021-10-31 23:04:31 +0700

    add: setup

---
+++ 0_setup/index.ts
@@ -0,0 +1,7 @@
+// yarn init
+// yarn add typescript -D
+// yarn add ts-node -D
+// yarn add @types/node
+// ?npx tsc -init // create tsconfig
+// ?npx ts-node index.ts // run ts
+// ?npx tsc index.ts //compile to js
